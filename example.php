b202a0418bbd08dacf9bbf6e70d6e27b9825faca
vorona-yurii <voronyu1331@gmail.com>
2022-09-08 13:25:36 +0300

    init

---
+++ example.php
@@ -0,0 +1,14 @@
+<?php
+
+$data = [
+    'user_id' => 123,
+    'room' => 321,
+    'EIO' => 3,
+];
+$address = '/socket.io/?' . http_build_query($data);
+$io = new SocketIOConnect('127.0.0.1', 8080, $address);
+$io->send('chat message', json_encode([
+    'chat_id' => 1,
+    'user_id' => 123,
+    'message' => 'Test message'
+]));
